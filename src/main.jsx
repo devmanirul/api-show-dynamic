5dbbd4c22acb3998b16c37a2dd1bb34d8c6e1b51
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-11-11 18:36:20 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import routers from "./routes/index.jsx";
 import ProductContextProvider from "./context/ProductContext.jsx";
- 
 
 createRoot(document.getElementById("root")).render(
   <ProductContextProvider>
